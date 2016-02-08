a0e3e3877ea449f1bc2de8394012765e6220a249
altescape <hello@michaelwatts.me>
2016-02-08 09:49:14 +0000

    add t and m to phone numbers

--- signature-maker.rb
+++ signature-maker.rb
@@ -48,9 +48,9 @@ html = <<EOF
         
         <tr style="margin:0;padding:0;color:#b0a49b;">
           <td style="margin:0;padding:0;padding-top:8px;font-family:'Lucida Grande',sans-serif;white-space:nowrap;">
-            <span style="color:#b0b0b0">+44(0)1252 715 059</span>
+            <span style="color:#b0b0b0">T: +44(0)1252 715 059</span>
             <span style="color:#27AAE2; margin: 0 10px">•</span>
-            <span style="color:#b0b0b0">#{mobile}</span>
+            <span style="color:#b0b0b0">M: #{mobile}</span>
             <span style="color:#27AAE2; margin: 0 10px">•</span>
             <a href="http://smswmedia.com" style="border:none;text-decoration:none;color:#b0b0b0;">www.smswmedia.com</a><br>
           </td>
